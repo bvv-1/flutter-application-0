97d87301b040ae541606b1de5f00ced105fee13f
bvv-1 <fhinnoigsr@gmail.com>
2023-08-27 13:00:05 +0900

    init commit

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search Bar Sample')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SearchAnchor(
+            builder: (BuildContext context, SearchController controller) {
+          return SearchBar(
+            controller: controller,
+            padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0)),
+            onTap: () {
+              controller.openView();
+            },
+            onChanged: (_) {
+              controller.openView();
+            },
+            leading: const Icon(Icons.search),
+          );
+        }, suggestionsBuilder:
+                (BuildContext context, SearchController controller) {
+          return List<ListTile>.generate(5, (int index) {
+            final String item = 'item $index';
+            return ListTile(
+              title: Text(item),
+              onTap: () {
+                // setState(() {
+                //   controller.closeView(item);
+                // });
+              },
+            );
+          });
+        }),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/order.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ご注文内容'),
+      ),
+      body:
+          const Center(child: Text('ご注文画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
