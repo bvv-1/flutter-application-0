ec3f97c70683bfbd8147956f77c9a0a9f09372da
bvv-1 <fhinnoigsr@gmail.com>
2023-08-27 14:54:13 +0900

    Update home.dart

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -10,90 +10,94 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ホーム')),
-      body: Column(children: [
-        SearchAnchor(builder: (
-          BuildContext context,
-          SearchController controller,
-        ) {
-          return SearchBar(
-            controller: controller,
-            padding: const MaterialStatePropertyAll<EdgeInsets>(
-                EdgeInsets.symmetric(horizontal: 16.0)),
-            onTap: () {
-              controller.openView();
-            },
-            onChanged: (_) {
-              controller.openView();
-            },
-            leading: const Icon(Icons.search),
-          );
-        }, suggestionsBuilder: (
-          BuildContext context,
-          SearchController controller,
-        ) {
-          return List<ListTile>.generate(5, (int index) {
-            final String item = 'item $index';
-            return ListTile(
-              title: Text(item),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SearchAnchor(builder: (
+            BuildContext context,
+            SearchController controller,
+          ) {
+            return SearchBar(
+              controller: controller,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0)),
               onTap: () {
-                // setState(() {
-                //   controller.closeView(item);
-                // });
+                controller.openView();
               },
+              onChanged: (_) {
+                controller.openView();
+              },
+              leading: const Icon(Icons.search),
             );
-          });
-        }),
-        Column(
-          children: [
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("ジャンルから選ぶ",
-                  style: TextStyle(fontSize: 20), textAlign: TextAlign.left),
-            ),
-            const Divider(),
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4, // 1行に表示するアイテムの数
-                mainAxisExtent: 120,
+          }, suggestionsBuilder: (
+            BuildContext context,
+            SearchController controller,
+          ) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  // setState(() {
+                  //   controller.closeView(item);
+                  // });
+                },
+              );
+            });
+          }),
+          Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("ジャンルから選ぶ",
+                    style: TextStyle(fontSize: 20), textAlign: TextAlign.left),
               ),
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return const CardSmall(
+              const Divider(),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, // 1行に表示するアイテムの数
+                  mainAxisExtent: 120,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return const CardSmall(
+                      imageUrl: "https://via.placeholder.com/300/09f/fff.png",
+                      text: "中華料理");
+                },
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "注目の飲食店",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              const Divider(),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // 1行に表示するアイテムの数
+                  mainAxisExtent: 180,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return const CardLarge(
                     imageUrl: "https://via.placeholder.com/300/09f/fff.png",
-                    text: "中華料理");
-              },
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "注目の飲食店",
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.left,
+                    text: "有名町中華",
+                  );
+                },
               ),
-            ),
-            const Divider(),
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // 1行に表示するアイテムの数
-                mainAxisExtent: 180,
-              ),
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return const CardLarge(
-                  imageUrl: "https://via.placeholder.com/300/09f/fff.png",
-                  text: "有名町中華",
-                );
-              },
-            ),
-          ],
-        )
-      ]),
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
